77d73a142d968c83dd5b90b7618c01488f101537
hizkiawdev <hizkiawdev@gmail.com>
2025-07-16 13:46:22 +0700

    remove example section and update menu section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { gsap, ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
-import Example from "./components/Example";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
@@ -18,10 +17,6 @@ const App = () => {
       <About />
       <Art />
       <Menu />
-      <Example />
-      <div className="flex-center h-screen">
-        <h1 className="text-3xl font-mono font-normal">GSAP App</h1>
-      </div>
     </main>
   );
 };
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -14,7 +14,6 @@ const Menu = () => {
         trigger: "#menu",
         start: "45% center",
         end: "bottom bottom",
-        markers: true,
       },
     });
 
